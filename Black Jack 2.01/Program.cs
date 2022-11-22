3ddf2355b88fe4f9e1807e3d1c647d2a288b421e
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-11-22 19:54:20 +0100

    Börjat på nytt Black jack spel, använder whlie då det funkar bättre en det förra jag använde.

--- Black Jack 2.01/Program.cs
+++ Black Jack 2.01/Program.cs
@@ -11,20 +11,26 @@ namespace ____
 
             Random random = new Random();
 
-            int rndmvalue = number[random.Next(0, 11)];
-            string rndmsuit = suits[random.Next(0,4)];
-
-            //Behövs bara för första rundan
-            int rndmvalue2 = number[random.Next(0,11)];
-            string rndmsuit2 = suits[random.Next(0,4)];
 
             bool något = true;
 
             while (något = true)
             {
+                int rndmvalue = number[random.Next(0, 11)];
+                string rndmsuit = suits[random.Next(0, 4)];
+
+                //Behövs bara för första rundan
+                int rndmvalue2 = number[random.Next(0, 11)];
+                string rndmsuit2 = suits[random.Next(0, 4)];
+                int rdmvalue3 = number[random.Next(0, 11)];
+                int rdmvalue4 = number[random.Next(0, 11)];  
+
                 Console.WriteLine("Välkommen till Black Jack");
                 Console.WriteLine($"Dinna startkort kommer vara {rndmsuit} {rndmvalue} och {rndmsuit2} {rndmvalue2}");
                 Console.WriteLine($"Du har nu {rndmvalue + rndmvalue2} poäng");
+                Console.WriteLine($"Datorn fick {rdmvalue4 + rdmvalue3} poäng");
+                Console.WriteLine("Vill du köra igen (y/n)");
+                string svar = Console.ReadLine();
 
             }
 
